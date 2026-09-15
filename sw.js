166e7d64e14729dc23cf3b465068de23ca07db03
ferrempro <ferrempro@gmail.com>
2026-09-15 03:29:57 -0700

    Revert "Update sw.js"
    This reverts commit 74ea25f6164193c158c06baa1a5138acf5344ee9.

--- sw.js
+++ sw.js
@@ -35,11 +35,6 @@ self.addEventListener('fetch', event => {
         const copy = response.clone();
         caches.open(CACHE).then(cache => cache.put(event.request, copy));
         return response;
-        .catch(() =>
-  caches.match(event.request).then(cached =>
-    cached || caches.match('./index.html')
-  )
-)
       }).catch(() => caches.match('./index.html'))
     )
   );
